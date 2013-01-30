e37663432939b556402d275c40450f3d441fb355
miio <info@miio.info>
2013-01-30 23:35:25 +0900

    change target column to ovverride. and add ignore

--- README.md
+++ README.md
@@ -22,9 +22,32 @@ FromClass.relation.pickin(IntoClass, {column: value or column})
 
 Example:
 
-Map.where(stage_id: id).pickin(GameMap, {game_stage_id: @res.id, type: :type, x: :x, y: :y, created_at: :created_at, updated_at: :updated_at})
+### for over version 0.9.0
 
-INSERT INTO 'game_maps' ("game_stage_id", "type", "x", "y", "created_at", "updated_at") SELECT 24, type, x, y, created_at, updated_at FROM "maps" WHERE "maps"."stage_id" = 1
+  Map.where(stage_id: 1).pickin(GameMap, {game_stage_id: 24}, [:id])
+  
+    INSERT INTO 'game_maps' ("id", "game_stage_id", "type", "x", "y", "created_at", "updated_at") SELECT NULL, '24', "type", "x", "y", "created_at", "updated_at" FROM "maps" WHERE "maps"."stage_id" = 1
+
+  Map.where(stage_id: 1).select([:type]).pickin(GameMap, {game_stage_id: 24}, [:id])
+  
+    INSERT INTO 'game_maps' ("type") SELECT "type" FROM "maps"  WHERE "maps"."stage_id" = 1
+
+### for only version 0.0.1
+
+  Map.where(stage_id: 1).pickin(GameMap, {game_stage_id: 24, type: :type, x: :x, y: :y, created_at: :created_at, updated_at: :updated_at})
+  
+  	INSERT INTO 'game_maps' ("game_stage_id", "type", "x", "y", "created_at", "updated_at") SELECT 24, type, x, y, created_at, updated_at FROM "maps" WHERE "maps"."stage_id" = 1
+
+## Change log
+
+### version 0.9.0
+
+* Release beta
+* Specification was changed. The specified column was targeted. but now used to override the value of a column in the SELECT for.
+
+### version 0.0.1
+
+* Release alpha
 
 ## Contributing
 
--- lib/activerecord-pickin/relation.rb
+++ lib/activerecord-pickin/relation.rb
@@ -2,18 +2,50 @@ require 'active_record/relation'
 module ActiveRecord
   module Pickin
     module Relation
-      def pickin into_class, columns = {}
+      def pickin into_class, override = {}, ignore = []
         insert_arel = Arel::InsertManager.new(arel.engine)
         insert_arel.into into_class.table_name
-        if columns.empty?
-          insert_arel.ast.columns = into_class.map { |c| Arel::Attributes::Attribute.new insert_arel, c.name }
+
+        if self.select_values.empty?
+          columns = into_class.columns
         else
-          insert_arel.ast.columns = columns.keys.map { |c| Arel::Attributes::Attribute.new insert_arel, c }
+          columns = self.select_values
         end
-        columns.map { |c| c.to_s }
-        select_arel = self.select(columns.values)
+
+        insert_arel.ast.columns = columns.map do |c|
+          unless c.class == Symbol
+            c = c.name
+          end
+          Arel::Attributes::Attribute.new(insert_arel, c)
+        end
+
+        select_column = self.get_select_column columns, override, ignore
+        self.select_values = []
+        select_arel = self.select(select_column)
         @klass.connection.execute "#{insert_arel.to_sql} #{select_arel.to_sql}"
       end
+
+      def get_select_column target, override, ignore
+        target.map do |c|
+          unless c.class == Symbol
+            c = c.name.to_sym
+          end
+          convert_element(c, override) unless ignore.include? c
+        end
+      end
+
+      def convert_element column, override
+        if override.keys.include?(column)
+          if override[column].class == Symbol
+            @klass.connection.quote_column_name override[column]
+          else
+            "'#{override[column]}'"
+          end
+        else
+          @klass.connection.quote_column_name column.to_s
+        end
+      end
+
     end
   end
 end
